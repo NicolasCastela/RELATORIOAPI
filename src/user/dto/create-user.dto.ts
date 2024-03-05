928e2dd589776b321a833fae9e0aad9dcaec9c69
NicolasCastela <nickcastela2022@hotmail.com>
2024-03-05 12:00:37 -0400

    TESTE

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,27 @@
-export class CreateUserDto {    
+import { IsEmail, IsNotEmpty, IsNumber, IsString, Length } from "class-validator"
+
+export class CreateUserDto { 
+    
+    @IsEmail()
+    @IsString()
+    @IsNotEmpty()
     email: string 
+
+    @IsString()
+    @IsNotEmpty()
     nome: string
+    
+    @IsString()
+    @IsNotEmpty()
     senha: string
+
+    @Length(1, 11,
+         { message: 'O telefone deve ter no máximo 11 dígitos.' })
+    @IsNumber()
+    @IsNotEmpty()
     telefone:  number
+
+    @IsString()
+    @IsNotEmpty()
     cpf: string
 }
